90720c2996219031dffa420a4f22c313cc4c7bef
Davor Ambrus <Davor.Ambrus@endava.com>
2026-02-13 13:55:31 +0100

    trying beforeEach

--- tests/aboutus.spec.ts
+++ tests/aboutus.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("has about us", async ({ page }) => {
+test.beforeEach("Open start URL", async ({ page }) => {
+  console.log(`Running ${test.info().title}`);
   await page.goto("https://www.demoblaze.com/index.html");
+});
+
+test("has about us", async ({ page }) => {
+  //await page.goto("https://www.demoblaze.com/index.html");
   await page.getByRole("link", { name: "About us" }).click();
   // Expect a form to have input fields.
   await expect.soft(page.locator(".vjs-poster")).toBeVisible();
