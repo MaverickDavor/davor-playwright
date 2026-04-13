f261400f494d22fe9cc3ebcad73bb3efbd1749b2
Davor Ambrus <Davor.Ambrus@endava.com>
2026-04-13 14:48:51 +0200

    Final report fix #4

--- tests/end2end/add-to-cart.spec.ts
+++ tests/end2end/add-to-cart.spec.ts
@@ -6,6 +6,7 @@ import { test } from "../../fixtures/basePage";
 test.describe("Adding to Cart second", () => {
   test("add to bag", async ({ addToCartPage, cartPage, page }) => {
     await addToCartPage.goto(page);
+
     // Expect a link "to be named" a substring.
     await cartPage.addNexus.click({ force: true });
     page.on("dialog", async (dialog) => {
